43e496eb8abd84741215e52dc579e762b6c3a715
Tegzes Tamás <tegzes.tamas@gmail.com>
2018-08-26 14:20:44 +0200

    Add web download to RSSReader and demo in main.kt

--- src/main/kotlin/hu/tegzes/tamas/notifier/main.kt
+++ src/main/kotlin/hu/tegzes/tamas/notifier/main.kt
@@ -1,11 +1,19 @@
 package hu.tegzes.tamas.notifier
 
 import hu.tegzes.tamas.notifiers.PushbulletNotifier
+import hu.tegzes.tamas.readers.RSSReader
 import java.io.File
+import java.net.URL
 
 fun main(args: Array<String>) {
     println("Hello")
-    val apiKey = File("pushbullet_key.txt").bufferedReader().readLine()
-    PushbulletNotifier(apiKey).sendLink(url = "github.com", email = "tamastom96@gmail.com")
+    val rssReader = RSSReader(URL("https://www.mavcsoport.hu/mavinform/rss.xml"))
+    val bufferedReader = File("pushbullet_key.txt").bufferedReader()
+    val apiKey = bufferedReader.readLine()
+    bufferedReader.close()
+    val text = rssReader.update()
+    if (text != null) {
+        PushbulletNotifier(apiKey).sendNote(body = text, email = "tamastom96@gmail.com")
+    }
 
 }
--- src/main/kotlin/hu/tegzes/tamas/readers/RSSReader.kt
+++ src/main/kotlin/hu/tegzes/tamas/readers/RSSReader.kt
@@ -2,16 +2,16 @@ package hu.tegzes.tamas.readers
 
 import com.github.magneticflux.rss.createRssPersister
 import com.github.magneticflux.rss.namespaces.standard.elements.Rss
-import java.io.File
 import java.net.URL
 
 class RSSReader(val url: URL) {
     fun update(): String? {
         val persister = createRssPersister()
-        val root = persister.read(Rss::class.java, File("rss.xml"))
+        val connection = url.openConnection()
+        val root = persister.read(Rss::class.java, connection.getInputStream())
         for (item in root.channel.items) {
             println(item.title)
         }
-        return null
+        return root.channel.items.first().title
     }
 }
