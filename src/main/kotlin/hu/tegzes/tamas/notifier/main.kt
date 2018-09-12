e23ac9a3b4e70fb7499d6a297fc151ebe464c8ba
Tegzes Tamás <tegzes.tamas@gmail.com>
2018-09-12 10:01:58 +0200

    Create rough schema

--- src/main/kotlin/hu/tegzes/tamas/notifier/main.kt
+++ src/main/kotlin/hu/tegzes/tamas/notifier/main.kt
@@ -8,70 +8,50 @@ import java.sql.Connection
 
 
 fun main(args: Array<String>) {
-    Database.connect("jdbc:sqlite:/media/LinuxData/sqlite_db/test.db", "org.sqlite.JDBC")
+    Database.connect("jdbc:sqlite:/home/tamas/SourceCodes/notifier/db/notifier.db", "org.sqlite.JDBC")
     TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
 
     transaction {
         addLogger(StdOutSqlLogger)
+        SchemaUtils.create(RssReaderTable, WebReaderTable, ReaderTypeTable, ReaderTable, NotifierTypeTable, NotifierTable, SubscriptionTable)
 
-        SchemaUtils.create(Cities, Residents)
-
-        val stPeteId = Cities.insert {
-            it[name] = "St. Petersburg"
-        } get Cities.id
-
-        val budapestId = Cities.insert {
-            it[name] = "Budapest"
-        } get Cities.id
-
-        if (stPeteId != null) {
-
-            Residents.insert {
-                it[name] = "Vladimir"
-                it[city] = stPeteId
-            }
-
-            Residents.insert {
-                it[name] = "Vladimirovic"
-                it[city] = stPeteId
-            }
-
-            Residents.insert {
-                it[name] = "Vladimirovicovic"
-                it[city] = stPeteId
-            }
-        }
-        if (budapestId != null) {
-            Residents.insert {
-                it[name] = "Kovács"
-                it[city] = budapestId
-            }
-
-            Residents.insert {
-                it[name] = "Kovácsné"
-                it[city] = budapestId
-            }
-
-            Residents.insert {
-                it[name] = "ifj. Kovács"
-                it[city] = budapestId
-            }
-        }
-
-        println("Id of St. Petersburg: $stPeteId")
-
-        for (row in (Cities innerJoin Residents).selectAll()) {
-            println(row)
-        }
 
     }
 }
 
-object Cities : IntIdTable() {
-    val name = varchar("name", 50)
+object RssReaderTable : Table() {
+    val url = varchar("url", 100)
+    val titleFilter = varchar("titleFilter", 100)
+    val descFilter = varchar("descFilter", 100)
+    val lastMatchTime = datetime("lastMatchTime")
+    val readerId = (entityId("readerId", ReaderTable) references ReaderTable.id).primaryKey()
 }
 
-object Residents : IntIdTable() {
-    val city = entityId("city", Cities) references Cities.id
-    val name = text("name")
+object WebReaderTable : IntIdTable("WebReader") {
+    val url = varchar("url", 100)
+    val lastContent = text("lastContent")
+    var readerId = (entityId("readerId", ReaderTable) references ReaderTable.id).primaryKey()
 }
+
+object ReaderTypeTable : IntIdTable("ReaderType") {
+    val name = varchar("name", 40)
+}
+
+object ReaderTable : IntIdTable("Reader") {
+    val type = entityId("type", ReaderTypeTable) references ReaderTypeTable.id
+}
+
+object NotifierTypeTable : IntIdTable("NotifierType") {
+    val name = varchar("name", 40)
+}
+
+object NotifierTable : IntIdTable("Notifier") {
+    val type = entityId("type", NotifierTypeTable) references NotifierTypeTable.id
+
+}
+
+object SubscriptionTable : Table("Subscription") {
+    val notifierId = entityId("notifierId", NotifierTable) references NotifierTable.id
+    val readerId = entityId("readerId", ReaderTable) references ReaderTable.id
+}
+
