c6897c7e70df0ddea8c9fadde2629dc6f8a1e59c
Tegzes Tamás <tegzes.tamas@gmail.com>
2019-01-15 14:25:02 +0100

    Allow empty initialization and subscribe/unsubscribe to a Reader

--- src/main/kotlin/hu/tegzes/tamas/notifier/readers/Reader.kt
+++ src/main/kotlin/hu/tegzes/tamas/notifier/readers/Reader.kt
@@ -3,7 +3,17 @@ package hu.tegzes.tamas.notifier.readers
 import hu.tegzes.tamas.notifier.notifiers.Notifier
 import hu.tegzes.tamas.notifier.notifiers.NotifierVisitor
 
-abstract class Reader(private val notifiers: List<Notifier>) : NotifierVisitor {
+abstract class Reader() : NotifierVisitor {
+    private val notifiers = mutableSetOf<Notifier>()
+
+    fun subscribe(notifier: Notifier) {
+        notifiers.add(notifier)
+    }
+
+    fun unsubscribe(notifier: Notifier) {
+        notifiers.remove(notifier)
+    }
+
     fun update() {
         if (checkForUpdates()) {
             for (notifier in notifiers) {
