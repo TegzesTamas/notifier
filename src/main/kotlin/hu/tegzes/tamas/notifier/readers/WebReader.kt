726cd7bec75a3b1ea5ff9ce2d88989daf037f109
Tegzes Tamás <tegzes.tamas@gmail.com>
2019-01-15 20:54:52 +0100

    Make the lastContent attribute of WebReader nullable

--- src/main/kotlin/hu/tegzes/tamas/notifier/readers/WebReader.kt
+++ src/main/kotlin/hu/tegzes/tamas/notifier/readers/WebReader.kt
@@ -4,7 +4,7 @@ import hu.tegzes.tamas.notifier.notifiers.EmailNotifier
 import hu.tegzes.tamas.notifier.notifiers.PushbulletNotifier
 import org.jetbrains.exposed.dao.EntityID
 
-class WebReader(val id: EntityID<Int>, val url: String, val lastContent: String) : Reader() {
+class WebReader(val id: EntityID<Int>, val url: String, val lastContent: String?) : Reader() {
     override fun checkForUpdates(): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
