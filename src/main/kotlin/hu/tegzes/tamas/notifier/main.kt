96d9896fa81ce5895fdbdfcc69607cf452a0aa71
Tamás Tegzes <tegzes.tamas@gmail.com>
2019-01-23 18:23:56 +0100

    Refactor database handling

--- src/main/kotlin/hu/tegzes/tamas/notifier/main.kt
+++ src/main/kotlin/hu/tegzes/tamas/notifier/main.kt
@@ -1,24 +1,9 @@
 package hu.tegzes.tamas.notifier
 
-import hu.tegzes.tamas.notifier.database.addEmailNotifier
-import hu.tegzes.tamas.notifier.database.addRssReader
-import hu.tegzes.tamas.notifier.database.addSubscription
-import hu.tegzes.tamas.notifier.database.init
+import hu.tegzes.tamas.notifier.database.DataAccess
 
 
 fun main(args: Array<String>) {
-    val (readers, notifiers) = init()
-    val newReader = addRssReader("https://www.mavcsoport.hu/mavinform/rss.xml", null, null)
-    val newNotifierPair = addEmailNotifier("tamastom96@gmail.com")
-    if (newReader != null && newNotifierPair != null) {
-        readers[newReader.id] = newReader
-        val (newNotifierId, newNotifier) = newNotifierPair
-        notifiers[newNotifierId] = newNotifier
-        addSubscription(newReader.id, newNotifierId)
-        newReader.subscribe(newNotifier)
-        newReader.update()
-    }
-    for (reader in readers.values) {
-        reader.update()
-    }
+    val readers = DataAccess.readers
+    readers.values.forEach { it.update() }
 }
